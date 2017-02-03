7e62f2782e7cad1ea7e9118cb5e85a1e484bd523
Jeevithagv2016 <jeevigv2016@gmail.com>
2017-02-03 14:26:11 +0530

    Create search.c

---
+++ search.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[10],b[10];
+	int i,j,n,m;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	scanf("%d",&m);
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]==m)
+	    {
+	        printf("%d",i);
+	        break;
+	    }
+	}
+	        
+	return 0;
+}
+
