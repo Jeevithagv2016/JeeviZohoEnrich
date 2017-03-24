344f86730640bb2eee4656e5e17182ef3e009f4c
Jeevithagv2016 <jeevigv2016@gmail.com>
2017-03-24 10:55:52 +0530

    Update spiral.c

--- spiral.c
+++ spiral.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
-
 int main(void) {
-    int i,j,n;
-    scanf("%d",&n);
+    int i,j,n,k=1;
+    int a[10][10];
+    scanf("%d",&n);//no of rows and columns
     for(i=0;i<n;i++)
     {
+        for(j=0;j<n;j++)
+        {
+            a[i][j]=k;
+            k++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
             for(j=0;j<n;j++)
             {
-
-            if(i%2==0)
-            {
-                printf("%d",j);
-            } 
-        
-            else
-            {
-                printf("%d",j);
+                printf("%d ",a[i][j]);
             }
-            printf("\n");
         }
-        
+        else
+        {
+            for(j=n-1;j>=0;j--)
+            {
+                printf("%d ",a[i][j]);
+            } 
+        }    
+        printf("\n");
     }
 	return 0;
 }
 
+
+    
